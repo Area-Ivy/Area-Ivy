625c52e1e45c2fa54284c951f340f6ec29431c55
Chen <1776672868@qq.com>
2025-05-27 13:01:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,13 @@ const readme = `\
 ### 👨‍💻 *About Me*
 
 * 🚀 I'm an undergraduate student of School of Software Engineering, [Tongji University](https://www.tongji.edu.cn).
+
 * 🌱 I’m currently learning software engineering and exploring new technologies.
+
 * 🔭 I love collaborating on interesting tech projects and connecting with like-minded individuals.
+
 * 📫 Email: chenyiming12500@gmail.com
+
 * 💡 OnlyFans: not yet available
 
 ### 📊 *My GitHub*
